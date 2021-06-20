e4f78e1c2c30d75854df3f483d755906aff330c1
Alex Machado <amachado@sva.edu>
2021-06-20 17:32:24 -0400

    Start test build for final project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import axios from 'axios';
+
 function App() {
+
+  const [activity, setActivity] = useState('');
+  const [title, setTitle] = useState('');
+
+  const fetchData = async () =>{
+    await axios.get('https://www.boredapi.com/api/activity')
+    .then(res => {
+      setActivity(res.data.activity + '.');
+      setTitle(res.data.type);
+    })
+    
+  }
+
+  let inspiration;
+
+  if (title === ''){
+    inspiration = <h3>Click the button to get started...</h3>
+  } else {
+    inspiration =  <h3>{title}</h3>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, [name]</h1>
+      <nav>
+        <li>Card</li>
+        <li>You</li>
+      </nav>
+
+      <div>
+        {inspiration}
+        <p>{activity}</p>
+        <button onClick={() => fetchData()}>Get inspired</button>
+      </div>
+      
     </div>
   );
 }
